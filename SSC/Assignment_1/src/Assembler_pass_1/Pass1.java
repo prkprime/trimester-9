cb675caf0d74ae54171551add051f13bb425503c
B04L0514 <ibm@b04l0514.mitwpu.edu.in>
2020-03-12 15:28:59 +0530

    signing off from lab pc

--- SSC/Assignment_1/src/Assembler_pass_1/Pass1.java
+++ SSC/Assignment_1/src/Assembler_pass_1/Pass1.java
@@ -1,4 +1,4 @@
-package Assembler_pass_1;
+//package Assembler_pass_1;
 
 import java.io.BufferedReader;
 import java.io.FileReader;
@@ -7,7 +7,7 @@ import java.util.StringTokenizer;
 
 public class Pass1 {
     public static void main(String[] args) throws IOException {
-        String optab = {
+        String[][] optab = {
           {"stop", "IS 00"},
           {"add","IS 01"},
           {"sub","IS 02"},
@@ -26,30 +26,33 @@ public class Pass1 {
           {"origin","AD 03"},
           {"equ","AD 04"},
           {"ltorg","AD 05"}
-        }
-        String regtab = {
+        };
+        String[][] regtab = {
           {"areg", "01"},
           {"breg", "02"},
           {"creg", "03"},
           {"dreg", "04"}
-        }
+        };
         BufferedReader br = new BufferedReader(new FileReader("../mlp.asm"));
         String thisLine;
         int lineNo = 0;
         while((thisLine = br.readLine()) != null) {
             StringTokenizer st = new StringTokenizer(thisLine, " ,");
+	    int isStart = 0;
             while (st.hasMoreTokens()) {
-                token = st.nextToken()
-                for (string[] element : optab) {
-                  if (token.toLowerCase() == element[0]) {
-                    System.out.print(element[1])
+                String token = st.nextToken();
+                for (String[] element : optab) {
+                  if (token.toLowerCase().equals(element[0])) {
+                    System.out.print(element[1]);
                   }
-                  for (string[] element : optab) {
-                    if (token.toLowerCase() == element[0]) {
-                      System.out.print(element[1])
-                    }
+		}
+                for (String[] element : regtab) {
+                  if (token.toLowerCase().equals(element[0])) {
+                    System.out.print(" "+element[1]);
+                  }
                 }
             }
+	    System.out.print("\n");
         }
     }
 }
